0800deb66f3580e07052d07223645284b41a896e
mariialobur <mariialobur545@gmail.com>
2026-08-19 17:43:50 +0200

    Add Level 1 atelier loader

---
+++ atelier-loader.js
@@ -0,0 +1,8 @@
+if(!document.querySelector('link[data-atelier-practice]')){
+  const link=document.createElement('link');
+  link.rel='stylesheet';
+  link.href='./atelier-practice.css?v=1.0.0';
+  link.dataset.atelierPractice='1';
+  document.head.appendChild(link);
+}
+import './atelier-practice.js';
